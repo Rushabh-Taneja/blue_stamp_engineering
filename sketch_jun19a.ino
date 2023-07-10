f1d1bdda39455050965d26e03029be5341378de0
Rushabh-Taneja <107726702+Rushabh-Taneja@users.noreply.github.com>
2023-07-10 15:14:56 -0700

    Add files via upload

--- sketch_jun19a.ino
+++ sketch_jun19a.ino
@@ -1,6 +1,18 @@
 #include <Keypad.h>
 #include <Servo.h>
 #include <Adafruit_Fingerprint.h>
+#include <LiquidCrystal.h>
+#include <FastLED.h>
+
+#define LED_PIN     26
+#define NUM_LEDS    60
+#define BRIGHTNESS  64
+#define LED_TYPE    WS2811
+#define COLOR_ORDER GRB
+CRGB leds[NUM_LEDS];
+
+#define UPDATES_PER_SECOND 100
+
 
 #if (defined(__AVR__) || defined(ESP8266)) && !defined(__AVR_ATmega2560__)
 
@@ -8,12 +20,13 @@
 // pin #12 is IN from sensor (GREEN wire)
 // pin #13 is OUT from arduino  (WHITE wire)
 // Set up the serial port to use softwareserial..
-SoftwareSerial mySerial(12, 13);
+SoftwareSerial mySerial(18, 19);
 
 #else
 // On Leonardo/M0/etc, others with hardware serial, use hardware serial!
 // #0 is green wire, #1 is white
 #define mySerial Serial1
+
 
 #endif
 
@@ -22,6 +35,8 @@ const byte COLS = 3;  //three columns
 String password = "1234";
 String code = "";
 String input = "";
+
+
 
 Servo myservo;
 Adafruit_Fingerprint finger = Adafruit_Fingerprint(&mySerial);
@@ -42,8 +57,14 @@ byte colPins[COLS] = { 3, 4, 5 };     //connect to the column pinouts of the key
 //Create an object of keypad
 Keypad keypad = Keypad(makeKeymap(keys), rowPins, colPins, ROWS, COLS);
 
-void setup() {
 
+const int rs = A0, en = A1, d4 = A2, d5 = A3, d6 = A4, d7 = A5;
+
+LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
+bool i = true;
+
+
+void setup() {
 
   Serial.begin(9600);
   myservo.attach(11);  // attaches the servo on pin 11 to the servo object
@@ -62,139 +83,220 @@ void setup() {
     Serial.println("Did not find fingerprint sensor :(");
     while (1) { delay(1); }
   }
+  lcd.begin(16, 2);
+  // set up the LCD's number of columns and rows:
 
+  Hello();
 }
-
-
 //loop
 
 
 void loop() {
 
   char key = keypad.getKey();  // Read the key
+
+
   if (key == '*') {
     input = "";
     code = "";
+    lcd.clear();
+    lcd.print("cleared");
     Serial.println("cleared");
   }
 
   else if (key == '#') {
     if (password == input) {
-        input = "";
-        code = "";
+      input = "";
+      code = "";
       finger.LEDcontrol(FINGERPRINT_LED_GRADUAL_ON, 0, FINGERPRINT_LED_PURPLE);
+
+      lcd.clear();
+      lcd.print("put finger on");
+      lcd.setCursor(0, 1);
+      lcd.print("sensor ");
+      // lcd.setCursor(0, 1);
+
+
 
       Serial.println("put finger on sensor");
       delay(2000);
-      finger.LEDcontrol(FINGERPRINT_LED_GRADUAL_OFF, 0, FINGERPRINT_LED_PURPLE);
 
-      if (getFingerprintID() == 1) {
-        finger.LEDcontrol(FINGERPRINT_LED_GRADUAL_ON, 0, FINGERPRINT_LED_BLUE);
-        Serial.println("# to set new passcode");
+      if (getFingerprintID() == 20) {
+        lcd.clear();
+        finger.LEDcontrol(FINGERPRINT_LED_GRADUAL_OFF, 0, FINGERPRINT_LED_PURPLE);
+
+        finger.LEDcontrol(FINGERPRINT_LED_GRADUAL_ON, 0, FINGERPRINT_LED_BLUE, 200);
+
+        Serial.println("# to set new code");
         Serial.println("* to unlock or lock box");
         Serial.println("0 to add fingerprint");
 
-        char key = '\0';
-        while (1 == 1) {
+        key = '\0';
+        long startMillis = millis();
+        while (true) {
           key = keypad.getKey();
 
-          if (key != '\0' && key != '1' && key != '2' && key != '3' && key != '4' && key != '5' && key != '6' && key != '7' && key != '8' && key != '9') {
+          if (key == '0' || key == '#' || key == '*') {
             break;
+          }
+          long currentMillis = millis();
+          if (((currentMillis - startMillis) % 9000) < 3000) {
+            lcd.println("Press # to set            ");
+            lcd.setCursor(0, 1);
+            lcd.println("new Code              ");
+
+            lcd.clear();
+            // while(((currentMillis - startMillis) % 9000) < 3000){
+
+            // }
+          } else if (((currentMillis - startMillis) % 9000) < 6000) {
+            lcd.println("Press * to lock           ");
+            lcd.setCursor(0, 1);
+            lcd.println("or unlock box            ");
+            lcd.clear();
+            //             while(((currentMillis - startMillis) % 6000) < 3000){
+              
+            // }
+          } else if (((currentMillis - startMillis) % 9000) < 9000) {
+            lcd.println("Press 0 to add        ");
+            lcd.setCursor(0, 1);
+            lcd.println("fingerprint             ");
+            lcd.clear();
+            //             while(((currentMillis - startMillis) % 9000) < 9000){
+              
+            // }
           }
         }
 
+
         if (key == '#') {
-          Serial.println("Enter what you want the password to be");
+          lcd.println("Enter Code          ");
+          lcd.setCursor(0, 1);
+          lcd.println("#-confirm*-clear");
+
           Serial.println("Press # is to confirm");
           Serial.println("Press * is to clear");
 
           input = "";
-          code = "";
           key = '\0';
-          while (1 == 1) {
+          while (true) {
+            key = keypad.getKey();
+            if (key != '\0') {
+              input += key;
+              lcd.clear();
+              lcd.print(input);
+              break;
+            }
+          }
+          while (true) {
             key = keypad.getKey();
 
             if (key != '\0') {
               if (key == '#') {
                 password = input;
-                Serial.println("New password is: " + password);
+                Serial.println("New Code is: " + password);
+                lcd.clear();
+                lcd.println("New Code is:       ");
+                lcd.setCursor(0, 1);
+                lcd.println(password);
+                Hello();
+
                 input = "";
                 break;
               }
               input += key;
               Serial.println(input);
+              lcd.clear();
+              lcd.print(input);
+
 
               if (key == '*') {
                 input = "";
-                code = "";
                 Serial.println("cleared");
+                lcd.clear();
+                lcd.println("cleared");
+                lcd.setCursor(0, 1);
+                lcd.println("Enter new code");
               }
             }
           }
-          Serial.println("New password is: " + password);
+          Serial.println("New code is: " + password);
           delay(200);
           finger.LEDcontrol(FINGERPRINT_LED_GRADUAL_OFF, 0, FINGERPRINT_LED_BLUE);
         }
 
         else if (key == '*') {
+          if (i) {
+            Serial.println("Unlocked");
+            lcd.clear();
+            lcd.print("Unlocked");
+            i = false;
 
-          Serial.println("Unlocked");
-
-          if (pos == 0) {
-            for (pos = 0; pos < 90; pos += 3) {  // goes from 0 degrees to 180 degrees
-              // in steps of 1 degree
-              myservo.write(pos);  // tell servo to go to position in variable 'pos'
-              delay(15);           // waits 15ms for the servo to reach the position
-            }
+          } else {
+            Serial.println("Locked");
+            lcd.clear();
+            lcd.print("Locked");
+            i = true;
           }
 
-          else if (pos == 90) {
-            for (pos = 90; pos > 0; pos -= 3) {  // goes from 0 degrees to 180 degrees
-              // in steps of 1 degree
-              myservo.write(pos);  // tell servo to go to position in variable 'pos'
-              delay(15);           // waits 15ms for the servo to reach the position
-            }
-          }
-
+          lock();
           delay(2000);
           finger.LEDcontrol(FINGERPRINT_LED_GRADUAL_OFF, 0, FINGERPRINT_LED_BLUE);
+          Hello();
         }
 
         else if (key == '0') {
 
-          String placeholder = "";
-          int i=0;
+          code = "";
+
           key = '\0';
           Serial.println("put in a number between 1 and 127 for the fingerprint to be saved as");
-          Serial.println("press # to save the number");
+          lcd.clear();
+          //   lcd.print("put number 1-127 to save finger as");
+          lcd.print("save finger as #");
+          lcd.setCursor(0, 1);
+          lcd.print("from 1-127");
+
+          Serial.println("press # to save");
 
           while (true) {
             key = keypad.getKey();
-            int i = placeholder.toInt();
-            
-            Serial.println(i);
-            Serial.println(placeholder);
+
+            Serial.println(code);
 
             if (key == '#') {
-              if (placeholder.toInt() <= 127 && placeholder.toInt() > 0) {
-                id = placeholder.toInt();
+              if (code.toInt() <= 127 && code.toInt() > 0) {
+                id = code.toInt();
+                lcd.clear();
+                lcd.print("Put Finger on");
+                lcd.setCursor(0, 1);
+                lcd.print("Sensor");
                 while (!getFingerprintEnroll())
                   ;
+                lcd.clear();
+                lcd.print("Finger");
+                lcd.setCursor(0, 1);
+                lcd.print("Registered");
+                code = "";
+                Hello();
                 break;
-              }
-              else if(placeholder.toInt() == 0) {
+              } else if (code.toInt() == 0) {
                 break;
-              }e
-              else {
-                placeholder = "";
+              } else {
+                code = "";
                 Serial.println("Not a valid input");
+                lcd.print("Invalid Input");
+                lcd.setCursor(0, 1);
+                lcd.print("Reneter Code");
               }
-            }
-            else if (key == '*') {
-              placeholder = "";
-            }
-            else if(key){
-                placeholder += key;
+            } else if (key == '*') {
+              code = "";
+            } else if (key) {
+              code += key;
+              lcd.clear();
+              lcd.print(code);
+              lcd.setCursor(0, 1);
+              lcd.print("Press # to save");
             }
           }
         }
@@ -202,8 +304,13 @@ void loop() {
 
       else {
         Serial.println("Don't recognize finerprint");
+        lcd.clear();
+        lcd.print("Don't recognize");
+        lcd.setCursor(0, 1);
+        lcd.print("fingerprint");
+
         finger.LEDcontrol(FINGERPRINT_LED_GRADUAL_ON, 1, FINGERPRINT_LED_RED, 50);
-        delay(2000);
+        Hello();
         finger.LEDcontrol(FINGERPRINT_LED_GRADUAL_OFF, 0, FINGERPRINT_LED_RED);
 
         input = "";
@@ -213,9 +320,12 @@ void loop() {
     }
 
     else {
-      Serial.println("Wrong password");
       finger.LEDcontrol(FINGERPRINT_LED_GRADUAL_ON, 1, FINGERPRINT_LED_RED, 50);
-      delay(2500);
+      Serial.println("Wrong Code");
+      lcd.clear();
+      lcd.print("Wrong Code");
+      Hello();
+
       finger.LEDcontrol(FINGERPRINT_LED_GRADUAL_OFF, 0, FINGERPRINT_LED_RED);
       input = "";
       code = "";
@@ -224,12 +334,48 @@ void loop() {
 
   // Print if key pressed
   else if (key) {
-    code = code + "*";
-    input += key;
+    code += "*";
+    Serial.println(key);
     Serial.println(code);
+    Serial.println(code.length());
+    // input += key;
+    input = input + key;
+    Serial.println(input);
+    Serial.println(input.length());
+    Serial.println();
+
+    lcd.clear();
+    lcd.print(code);
   }
 }
 
+
+uint8_t lock() {
+  if (pos == 0) {
+    for (pos = 0; pos < 90; pos += 3) {  // goes from 0 degrees to 180 degrees
+      // in steps of 1 degree
+      myservo.write(pos);  // tell servo to go to position in variable 'pos'
+      delay(15);           // waits 15ms for the servo to reach the position
+    }
+  }
+
+  else if (pos == 90) {
+    for (pos = 90; pos > 0; pos -= 3) {  // goes from 0 degrees to 180 degrees
+      // in steps of 1 degree
+      myservo.write(pos);  // tell servo to go to position in variable 'pos'
+      delay(15);           // waits 15ms for the servo to reach the position
+    }
+  }
+}
+
+uint8_t Hello() {
+  delay(2000);
+  lcd.clear();
+  lcd.println("Hello                      ");
+  lcd.setCursor(0, 1);
+  lcd.println("Enter Code              ");
+  finger.LEDcontrol(FINGERPRINT_LED_OFF);
+}
 
 uint8_t getFingerprintEnroll() {
 
@@ -282,6 +428,8 @@ uint8_t getFingerprintEnroll() {
   }
 
   Serial.println("Remove finger");
+  lcd.clear();
+  lcd.print("Remove Finger");
   delay(2000);
   p = 0;
   while (p != FINGERPRINT_NOFINGER) {
@@ -290,7 +438,11 @@ uint8_t getFingerprintEnroll() {
   Serial.print("ID ");
   Serial.println(id);
   p = -1;
+  lcd.clear();
   Serial.println("Place same finger again");
+  lcd.print("Place Finger ");
+  lcd.setCursor(0, 1);
+  lcd.print("Again");
   while (p != FINGERPRINT_OK) {
     p = finger.getImage();
     switch (p) {
@@ -426,7 +578,7 @@ uint8_t getFingerprintID() {
   p = finger.fingerSearch();
   if (p == FINGERPRINT_OK) {
     Serial.println("Found a print match!");
-    return 1;
+    return 20;
   } else if (p == FINGERPRINT_PACKETRECIEVEERR) {
     Serial.println("Communication error");
     return p;
